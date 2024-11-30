9f650b93a371ddbe0910cae41b058347a0132c54
Roel U <roelul@outlook.com>
2024-11-30 09:00:46 -0700

    Solve excersise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(startNum,endNum) {
+    if(startNum<0 || endNum<0 
+        || Math.floor(startNum)!=startNum || Math.floor(endNum)!=endNum
+        || typeof(startNum)!="number" || typeof(endNum)!="number"
+        
+    ){
+        return 'ERROR'
+    }
+    
+    let sum=0
+    if(startNum<=endNum){
+        for(let num=startNum;num<=endNum;num++){
+            sum+=num
+        }
+        return sum
+    }
+    if(startNum>endNum){
+        for(let num=endNum;num<=startNum;num++){
+            sum+=num
+        }
+        return sum
+    }
 };
+
+console.log(sumAll(4,1))
+
 
 // Do not edit below this line
 module.exports = sumAll;
