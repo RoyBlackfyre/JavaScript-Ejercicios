bdf12bb3f83b55fa7a4fd972d2be5c89491cb656
Roel U <roelul@outlook.com>
2024-11-29 23:42:54 -0700

    Solved excersise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(ogArray,...numerosToRemove) {
+    let newArray=[]
+    for ( let element in ogArray){
+        if(!numerosToRemove.includes(ogArray[element])){
+            newArray.push(ogArray[element])
+        }
+    }
+    return newArray
 };
+
+console.log(removeFromArray([1,2,3,4],3,2,4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
