71792c05f47ea6ff7d459de66cba0cfcda7215a4
Roel U <roelul@outlook.com>
2024-11-29 22:34:54 -0700

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if(num<0){
+        return ('ERROR')
+    }else{
+        let theString = ""
+        for(let word=1;word<=num;word++){
+            theString += string
+        }
+        return theString
+    }
 
 };
 
